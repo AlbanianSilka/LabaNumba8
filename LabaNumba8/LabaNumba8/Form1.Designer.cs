bbfa3614c75d93e3e0bb2adeeedc22232c9f3df6
AlbanianSilka <58111574+AlbanianSilka@users.noreply.github.com>
2020-04-24 21:28:01 +0300

    Лаба номер 8
    Есть небольшой баг - если начать заправлять воду в резервуар и перед этим не нажав на кнопку "отправить" - значение, которое ввёл юзер обнуляется. С кнопкой рандомайзера воды в деревне похоже - там ставится рандомное число, но как только начинаешь отправлять в деревню воду - значение воды в деревне обнуляется.

---
+++ LabaNumba8/LabaNumba8/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace LabaNumba8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutTheProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetVillageWaterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.randomVillageWaterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(805, 439);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LabaNumba8.Properties.Resources.photo_2020_04_23_20_53_16;
+            this.pictureBox2.Location = new System.Drawing.Point(-3, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(793, 440);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(354, 132);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Заправить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(613, 309);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "/3000";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutTheProgramToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(787, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetVillageWaterToolStripMenuItem,
+            this.randomVillageWaterToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutTheProgramToolStripMenuItem
+            // 
+            this.aboutTheProgramToolStripMenuItem.Name = "aboutTheProgramToolStripMenuItem";
+            this.aboutTheProgramToolStripMenuItem.Size = new System.Drawing.Size(121, 20);
+            this.aboutTheProgramToolStripMenuItem.Text = "About the program";
+            this.aboutTheProgramToolStripMenuItem.Click += new System.EventHandler(this.aboutTheProgramToolStripMenuItem_Click);
+            // 
+            // resetVillageWaterToolStripMenuItem
+            // 
+            this.resetVillageWaterToolStripMenuItem.Name = "resetVillageWaterToolStripMenuItem";
+            this.resetVillageWaterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetVillageWaterToolStripMenuItem.Text = "Reset village water";
+            this.resetVillageWaterToolStripMenuItem.Click += new System.EventHandler(this.resetVillageWaterToolStripMenuItem_Click);
+            // 
+            // randomVillageWaterToolStripMenuItem
+            // 
+            this.randomVillageWaterToolStripMenuItem.Name = "randomVillageWaterToolStripMenuItem";
+            this.randomVillageWaterToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.randomVillageWaterToolStripMenuItem.Text = "Random village water";
+            this.randomVillageWaterToolStripMenuItem.Click += new System.EventHandler(this.randomVillageWaterToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 468);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutTheProgramToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetVillageWaterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem randomVillageWaterToolStripMenuItem;
+    }
+}
+
---
+++ LabaNumba8/LabaNumba8/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LabaNumba8
+{
+    public partial class Form1 : Form
+    {
+
+        public int water;
+        public int house_water = 0;
+
+
+        public void check_water()
+        {
+            if (Convert.ToInt32(water) + Convert.ToInt32(house_water) < 3000)  
+            {
+                String s = "WARNING, NOT ENOUGH WATER";
+                MessageBox.Show(s, "ERROR");
+            }
+            if (Convert.ToInt32(house_water) >= 3001){
+                String s = "В деревне уже достаточно воды, вы можете затопить её!";
+                MessageBox.Show(s, "ERROR");
+            }
+            else
+            {
+                timer2.Enabled = !timer2.Enabled;
+            }
+        }
+        public void send_button()
+        {
+            if (Convert.ToInt32(water) > 3000)
+            {
+                timer1.Stop();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                return;
+            }
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+            if (e.KeyChar == ',')
+            {
+                if (textBox1.Text.IndexOf(',') != 1)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            if (char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    button1.Focus();
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                water = Convert.ToInt32(textBox1.Text);
+                check_water();
+            }
+            catch
+            {
+                textBox1.Focus();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.textBox1.Text = (water++).ToString();
+            send_button();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (Convert.ToInt32(water) == 3000)
+            {
+                String s = "The water tank is already full";
+                MessageBox.Show(s, "Error");
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            this.label1.Text = (house_water++).ToString() + "/3000";
+            this.textBox1.Text = (water--).ToString();
+            if (Convert.ToInt32(house_water) > 3000)
+            {
+                timer2.Stop();
+                String s = "Congratulations! You sent the water into the village!";
+                MessageBox.Show(s, "Victory!");
+            }
+            else if (Convert.ToInt32(water) < 0)
+            {
+                timer2.Stop();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutTheProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String s = "В программе присутствуют две кнопки. Первая - количество воды, которое вводит юзер, второе - заправка резервуара, третье - вода в деревне.";
+            MessageBox.Show(s, "Instructions");
+        }
+
+        private void resetVillageWaterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Reset water", "Are you sure that you want to reset village water?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                this.label1.Text = (house_water = 0).ToString() + "/3000";
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+              
+            }
+        }
+
+        private void randomVillageWaterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Random water", "Are you sure that you want to randomize village water?", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Random rnd = new Random();
+                int house_water = rnd.Next(0, 3000);
+                this.label1.Text = (house_water).ToString() + "/3000";
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+
+            }
+        }
+    }
+}
